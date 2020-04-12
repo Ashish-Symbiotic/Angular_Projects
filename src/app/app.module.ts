8d8dfb3e6040f9fb4b9c3f36bf37e0a82855c8f9
Ashish <ashishpr78@gmail.com>
2020-04-12 15:24:52 +0530

    "New Compo Generated"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CasesComponent } from './cases/cases.component';
+import { CaseDetailsComponent } from './case-details/case-details.component';
+import { AddCasesComponent } from './add-cases/add-cases.component';
+import { EditCasesComponent } from './edit-cases/edit-cases.component';
+import { CasesStatComponent } from './cases-stat/cases-stat.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CasesComponent,
+    CaseDetailsComponent,
+    AddCasesComponent,
+    EditCasesComponent,
+    CasesStatComponent
   ],
   imports: [
     BrowserModule,
